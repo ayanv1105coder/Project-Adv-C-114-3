c6e22ddc551b6f5837efe32489444be844b7fd98
ayanv_supercoder_victorludorum <80285281+ayanv1105coder@users.noreply.github.com>
2021-11-18 07:28:47 +0000

    Add files via upload

---
+++ main.js
@@ -0,0 +1,42 @@
+mouthX=0;
+mouthY=0;
+
+function preload() {
+    moustache = loadImage("https://i.postimg.cc/3x3QzSGq/m.png");
+}
+
+function setup() {
+  canvas = createCanvas(300, 300);
+  canvas.center();
+  video = createCapture(VIDEO);
+  video.size(300, 300);
+  video.hide();
+
+  poseNet = ml5.poseNet(video, modelLoaded);
+  poseNet.on('pose', gotPoses);
+}
+
+function modelLoaded() {
+  console.log('PoseNet Is Initialized');
+}
+
+function gotPoses(results)
+{
+  if(results.length > 0)
+  {
+    console.log(results);
+    mouthX = results[0].pose.mouth.x;
+    mouthY = results[0].pose.mouth.y;
+    console.log("mouth x = " + mouthX);
+    console.log("mouth y = " + mouthY);
+  }
+}
+
+function draw() {
+  image(video, 0, 0, 300, 300);
+  image(moustache, mouthX, mouthY, 30, 30);
+}
+
+function take_snapshot(){    
+  save('myBeardedPhoto');
+}
